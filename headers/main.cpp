20c1e3600c2a69a6ae78e4df9cc9795af03c42a6
root <root@Notebook.Workgroup>
2015-03-18 14:56:46 -0300

    Exemplos de uso de arquivos de cabecalhos

---
+++ headers/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "soma.h"
+
+int main(){
+	using namespace std;
+	cout << "A soma de 3 e 4 é " << soma(3,4) << endl;
+	return 0;
+}
---
+++ headers/soma.h
@@ -0,0 +1,11 @@
+//esta é uma verificação se segurança, para evitar redeclarações
+//SOMA_H pode ter qualquer nome, desde que seja único.
+//Por convenção usamos o mesmo nome do arquio de cabecalho
+#ifndef SOMA_H
+#define SOMA_H
+	//este é o conteúdo do arquivo .h
+	//aqui colocamos as definições dos cabeçalhos de funções
+	int soma(int x, int y);
+
+//fim da verificação
+#endif
